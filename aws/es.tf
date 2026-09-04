74227b276b0a951da1d513236b12530d7d987b99
Eyas <emattar@paloaltonetworks.com>
2026-09-04 10:37:40 +0300

    op

--- aws/es.tf
+++ aws/es.tf
@@ -6,7 +6,7 @@ resource "aws_elasticsearch_domain" "monitoring-framework" {
     instance_type            = "t2.small.elasticsearch"
     instance_count           = 1
     dedicated_master_enabled = false
-    dedicated_master_type    = "m4.large.elasticsearch"
+    dedicated_master_type    = "m5.large.elasticsearch"
     dedicated_master_count   = 1
   }
 
